fbaaef6867f71165e13ff95b6efbfa71a0752f7f
JonatanVillegas <villegasjoncena7@gmail.com>
2025-09-12 12:42:57 -0600

    configuracion de gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider>
+      <Stack />;
+    </GluestackUIProvider>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,5 @@
+import { Box } from "@/components/ui/box";
+import { Button, ButtonSpinner, ButtonText } from "@/components/ui/button";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +12,12 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button className="p-3">
+      <ButtonSpinner color="gray" />
+      <ButtonText className="font-medium text-sm ml-2">
+       enviando solicitud 
+      </ButtonText>
+    </Button>
     </View>
   );
 }
